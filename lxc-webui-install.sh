9a74abe865cecd03e947342ffaba0d7e16b6c365
cn4096 <37654261@qq.com>
2026-06-24 22:38:24 +0800

    Create lxc-webui-install.sh

---
+++ lxc-webui-install.sh
@@ -0,0 +1,2 @@
+wget -O /usr/local/bin/service.set https://raw.githubusercontent.com/cn4096/service_set/main/service.set && chmod +x /usr/local/bin/service.set && echo "✅ 安装成功" || echo "❌ 安装失败"
+mkdir -p /opt/lxc-webui/
