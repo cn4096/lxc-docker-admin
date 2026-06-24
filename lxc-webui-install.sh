1382cde25af8f42a2d427d377b8d35d1a41c423e
cn4096 <37654261@qq.com>
2026-06-24 22:43:46 +0800

    Update lxc-webui-install.sh

--- lxc-webui-install.sh
+++ lxc-webui-install.sh
@@ -1,2 +1,6 @@
 wget -O /usr/local/bin/service.set https://raw.githubusercontent.com/cn4096/service_set/main/service.set && chmod +x /usr/local/bin/service.set && echo "✅ 安装成功" || echo "❌ 安装失败"
 mkdir -p /opt/lxc-webui/
+cd /opt/lxc-webui/
+wget -O lxc-webui https://raw.githubusercontent.com/cn4096/lxc-docker-admin/main/lxc-webui-linux-arm64-arm64
+chmod +x lxc-webui
+service.set -I lxc-webui
